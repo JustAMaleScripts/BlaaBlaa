2d40273975e2365127662f4c060e2183c2e83154
JustAMaleScripts <159631032+JustAMaleScripts@users.noreply.github.com>
2026-06-20 14:29:43 +0300

    Add files via upload

---
+++ community/Truly/Modules/Tank.lua
@@ -0,0 +1,33 @@
+-- UhhhhhhReanim/Modules/biast.lua
+local modules = {}
+table.insert(modules, function()
+    local m = {}
+    m.ModuleType  = "DANCE"
+    m.Name        = "Tank"
+    m.Description = "Should have used Cola,Vodka,Whisky for the song -TrulyAReal"
+    m.Assets = {"Tank.anim", "Tank.mp3"}
+    m.Config = function(parent)
+        Util_CreateText(parent, "No settings.", 14, Enum.TextXAlignment.Center)
+    end
+    m.SaveConfig = function() return {} end
+    m.LoadConfig  = function(save) end
+    local animator = nil
+    local start    = 0
+    m.Init = function(figure)
+        SetOverrideDanceMusic(AssetGetContentId("Tank.mp3"), "Tank", 0.8, NumberRange.new(0, 45.5))
+        start           = os.clock()
+        animator        = AnimLib.Animator.new()
+        animator.rig    = figure
+        animator.track  = AnimLib.Track.fromfile(AssetGetPathFromFilename("Tank.anim"))
+        animator.looped = true
+        animator.speed  = 1
+    end
+    m.Update = function(dt, figure)
+        animator:Step(os.clock() - start)
+    end
+    m.Destroy = function(figure)
+        animator = nil
+    end
+    return m
+end)
+return modules
