2eac99d4a3e4b12eba6299e9149d6d2b0641ceee
JustAMaleScripts <159631032+JustAMaleScripts@users.noreply.github.com>
2026-08-19 21:53:14 +0300

    Updated banned.lua

--- source/banned.lua
+++ source/banned.lua
@@ -1,23 +1,24 @@
 local banned = {
-  {UserId = 1, Reason = "Pedoblox"}
-  {UserId = 7046815862, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"}
-  {UserId = 11361264488, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"}
-  {UserId = 4614093134, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"}
-  {UserId = 9417641293, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"}
-  {UserId = 3184150217, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"}
-  {UserId = 11331155111, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"}
-  {UserId = 11357002447, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"}
-  {UserId = 7721835648, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"}
-  {UserId = 11427338289, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"}
-  {UserId = 8501284128, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"}
-  {UserId = 10856163248, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"}
-  {UserId = 2623885770, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"}
-  {UserId = 10771101554, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"}
-  {UserId = 2300528788, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"}
-  {UserId = 8204209950, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"}
-  {UserId = 9157307256, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"}
-  {UserId = 2510070060, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"}
-  {UserId = 8866839839, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"}
+  {UserId = 1, Reason = "Pedoblox"},
+  {UserId = 7046815862, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"},
+  {UserId = 11361264488, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"},
+  {UserId = 4614093134, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"},
+  {UserId = 9417641293, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"},
+  {UserId = 3184150217, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"},
+  {UserId = 11331155111, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"},
+  {UserId = 11357002447, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"},
+  {UserId = 7721835648, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"},
+  {UserId = 11427338289, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"},
+  {UserId = 8501284128, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"},
+  {UserId = 10856163248, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"},
+  {UserId = 2623885770, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"},
+  {UserId = 10771101554, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"},
+  {UserId = 2300528788, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"},
+  {UserId = 8204209950, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"},
+  {UserId = 9157307256, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"},
+  {UserId = 2510070060, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"},
+  {UserId = 8866839839, Reason = "Hacking the Game (Zombie Game UPD3). For Uhhhhhh Appeal: https://discord.gg/yCBNPnaymx"},
+  {UserId = 3118918860, Reason = "Test"}
 }
 
 return banned
